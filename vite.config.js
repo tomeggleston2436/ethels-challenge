da1204cbf5ad9e2b9756806a2c957ecec1cc7f6f
tomeggleston2436 <122117263+tomeggleston2436@users.noreply.github.com>
2024-02-21 18:18:59 +0000

    bugs

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,6 @@ import dotenv from 'dotenv';
 
 dotenv.config(); // Load environment variables
 
-/** @type {import('vite').UserConfig} */
-const config = {
-    plugins: [sveltekit()],
-    define: {
-        'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.SUPABASE_ANON_KEY)
-    }
-};
 
 export default defineConfig({
 	plugins: [sveltekit()]
