5b2ae68946a594f36bce932bbb50c0ea9df72675
tomeggleston2436 <122117263+tomeggleston2436@users.noreply.github.com>
2024-11-04 23:26:49 +0000

    link changes

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -16,7 +16,7 @@
         class="inline-block transition-opacity hover:opacity-90"
     >
         <img 
-            src="src/static/btn_strava_connectwith_orange.svg" 
+            src="/btn_strava_connectwith_orange.svg" 
             alt="Connect with Strava"
             class="h-12"
         />
@@ -24,7 +24,7 @@
 
     <div class="mt-8">
         <img 
-            src="src/static/api_logo_pwrdBy_strava_stack_gray.png"
+            src="/api_logo_pwrdBy_strava_stack_gray.png"
             alt="Powered by Strava"
             class="h-8 mx-auto"
         />
